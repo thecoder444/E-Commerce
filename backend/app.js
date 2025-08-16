d3e4aacfaf9800cc6765cd36c42898f97784d6f0
Ujjwal Mishra <155414589+thecoder444@users.noreply.github.com>
2025-08-16 09:01:16 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -18,15 +18,15 @@ app.use(express.json())
 app.use("/api", apiRouter)
 
 app.use(cors({
-<<<<<<< HEAD
-    origin: ['http://localhost:5000', 'http://localhost:5173','https://e-commerce-err0.onrender.com'],
-=======
-    origin: ['http://localhost:5000', 'http://localhost:5173', 'https://e-commerce-err0.onrender.com' ],
->>>>>>> 8f194511e711aa0c0e3b3b74d76158db49f3945e
+    origin: [
+        'http://localhost:5173', 
+        'https://e-commerce-frontend-cclz.onrender.com'
+    ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
 }));
+
 
 let port = process.env.PORT || 5000
 app.listen(port, ()=> {
