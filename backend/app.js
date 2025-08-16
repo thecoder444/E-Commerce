8f194511e711aa0c0e3b3b74d76158db49f3945e
Ujjwal Mishra <155414589+thecoder444@users.noreply.github.com>
2025-08-16 07:14:00 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -18,7 +18,7 @@ app.use(express.json())
 app.use("/api", apiRouter)
 
 app.use(cors({
-    origin: ['http://localhost:5000', 'http://localhost:5173'],
+    origin: ['http://localhost:5000', 'http://localhost:5173', 'https://e-commerce-err0.onrender.com' ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
