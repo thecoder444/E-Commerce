2a5512dae19bc8b0be69219b2b39a1f73abed7d9
Ujjwal Mishra <um1943238@gmail.com>
2025-08-16 07:29:53 +0530

    Updated frontend for production deployment

--- frontend/src/app/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit'
-import CartReducer from "../features/CartSlice/CartSlice"
-
-export const store = configureStore({
-  reducer: {
-    Cart: CartReducer
-  },
-})
